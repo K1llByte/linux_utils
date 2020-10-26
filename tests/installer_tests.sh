85398b10f9db8257bba6451de71181f62b3da367
KillByte <killbyte.prototype@gmail.com>
2020-10-26 17:56:37 +0000

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- tests/installer_tests.sh
+++ tests/installer_tests.sh
@@ -5,11 +5,30 @@ usage()
     echo "Usage: install [configs]"
 }
 
-install_configs()
-{
-    #CONFIGS=~/to/
-    eval "$(awk '/[ ]*#.*/{} /[^:]*:[ ]+[^ ]+[ ]+[^ ]+/{print "mkdir -p \"$(dirname \"",$3,"\") && cp -r",$2,$3,"&& echo \"Installed ",substr($1,1,length($1)-1),"\" config"}' configs_tests.txt)"
-}
+# install_configs()
+# {
+#     #CONFIGS=~/to/
+#     #[ -f arg ] && mkdir -p $(dirname "arg");[ -d arg ] && mkdir -p arg;
+#     #[ -f "" ] && mkdir -p $(dirname "");[ -d arg ] && mkdir -p "";
+    
+
+
+#     #MK_DIR_INE=""
+    
+#     AWK_SCRIPT='
+#     /[ ]*#.*/ {}
+
+#     function aux(str)
+#     {
+#         return "[ \"${asd: -1}\" = \"/\" ] && mkdir -p "str" || mkdir -p $(dirname "str")"
+#     }
+
+#     /[^:]*:[ ]+[^ ]+[ ]+[^ ]+/ {
+#         print aux($3)"; cp -r",$2,$3,"&& echo \"Installed "substr($1,1,length($1)-1)" config"
+#     }
+#     '
+#     awk "$AWK_SCRIPT" configs_tests.txt
+# }
 
 
 testing()
