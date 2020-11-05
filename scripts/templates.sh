63b0ca0367f69f33e4835f3f53b8827542a656c6
KillByte <killbyte.prototype@gmail.com>
2020-11-05 20:38:14 +0000

    + Templates prelude

---
+++ scripts/templates.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# settemplate [tag] [path/to/file] \ Set file as template
+# template [tag]                   \ Paste template
+# deltemplate [tag]                \ Delete template by name
+
+TEMPLATES=~/.config/templates/config
+
+# TODO: Templates namespaces 'template cpp::main'
