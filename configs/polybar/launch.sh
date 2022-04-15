eebe96eaf8df0ffae5f038110e7e7856b9d06e49
k1llbyte <killbyte.prototype@gmail.com>
2022-04-15 18:33:41 +0100

    Updated install script

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -7,8 +7,17 @@ launch_bar() {
     # Wait until the processes have been shut down
     while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+
     # Launch the bar
-    polybar -q main -c "$HOME/.config/polybar/shades/config.ini"
+    if type "xrandr"; then
+        # Lauch a bar for each monitor
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            MONITOR=$m polybar -q main -c "$HOME/.config/polybar/shades/config.ini" &
+        done
+    else
+        # If xrandr command doesn't exist just launch default bar
+        polybar -q main -c "$HOME/.config/polybar/shades/config.ini" &
+    fi
 }
 
 launch_bar
